64f9c4f72fe3b2e65c3f099740b8f614ff333f2b
romeollangozi <romeollangozi123@gmail.com>
2023-12-19 23:07:13 +0100

    s

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
-from flask import jsonify
 '''
 Module to create the routes
+for our api
 '''
+from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route("/status")
 def status():
+    '''
+    Status handler
+    '''
     return jsonify({"status": "OK"})
