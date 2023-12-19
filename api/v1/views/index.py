6fc8f17281994d7d58b154ed0341792b78764785
romeollangozi <romeollangozi123@gmail.com>
2023-12-19 23:21:07 +0100

    new api endpoint added for retrieving data about the number of instances

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,38 @@ for our api
 '''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route("/status")
 def status():
     '''
-    Status handler
+    Status endpoint
     '''
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    '''
+    Stats endpoint
+    '''
+    status = {}
+
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
