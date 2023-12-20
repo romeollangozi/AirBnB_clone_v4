8d59e6bd2f82add6082f4da64a36f62de08dd457
romeollangozi <romeollangozi123@gmail.com>
2023-12-21 00:12:30 +0100

    s

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,53 +8,60 @@ from flask import jsonify
 from flask import request
 from api.v1.views import app_views
 from flask import abort
+import json
 
 
-@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'])
-def return_states(state_id=None):
-    '''
-    Status endpoint
-    '''
-    if request.method == 'GET':
-        if state_id is None:
-            states = []
-            for obj in storage.all(State).values():
-                states.append(obj.to_dict())
-            return jsonify(states)
-        state = storage.get(State, state_id)
-        if state is None:
-            return abort(404)
-        return jsonify(state.to_dict())
-    if request.method == 'PUT':
-        state = storage.get(State, state_id)
-        if state is None:
-            return abort(404)
-        data = request.get_json(force=True)
-        if not data:
-            return abort(400, "Not a JSON")
-        for k, v in data.items():
-            if k not in ['id', 'updated_at', 'created_at']:
-                setattr(state, k, data[k])
-        storage.save()
-        return jsonify(state.to_dict())
-    if request.method == 'DELETE':
-        if state_id is None:
-            return abort(404)
-        obj = storage.get(State, state_id)
-        if obj is None:
-            return abort(404)
-        storage.delete(obj)
-        storage.save()
-        return jsonify({}), 200
-    if request.method == 'POST':
-        data = request.get_json(silent=True, force=True)
-        if not data:
-            return abort(400, "Not a JSON")
-        state_name = data.get("name", None)
-        if state_name is None:
-            return abort(400, "Missing name")
-        new_state = State(name=state_name)
-        storage.new(new_state)
-        storage.save()
-        return jsonify(new_state.to_dict()), 201
+@app_views.route('/states/', methods=['GET'])
+def list_states():
+    '''Retrieves a list of all State objects'''
+    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(list_states)
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    '''Retrieves a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
+        abort(404)
+    return jsonify(state_obj[0])
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    '''Deletes a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
+        abort(404)
+    state_obj.remove(state_obj[0])
+    for obj in all_states:
+        if obj.id == state_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
+@app_views.route('/states/', methods=['POST'])
+def create_state():
+    '''Creates a State'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    states = []
+    new_state = State(name=request.json['name'])
+    storage.new(new_state)
+    storage.save()
+    states.append(new_state.to_dict())
+    return jsonify(states[0]), 201
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def updates_state(state_id):
+    '''Updates a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    state_obj[0]['name'] = request.json['name']
+    for obj in all_states:
+        if obj.id == state_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(state_obj[0]), 200
