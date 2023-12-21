0af9b0efab048a6c5c60496ce5d5f669c95ab590
romeollangozi <romeollangozi123@gmail.com>
2023-12-21 18:42:32 +0100

    s

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ from models import storage
 from os import getenv
 from flask import jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, origins=['0.0.0.0'])
 app.register_blueprint(app_views)
 
 
